5c62ebd07577c2f2c65465aa52f0c847474bbf2e
Hack Reactor Student <student@hackreactor.com>
2018-01-18 15:02:23 -0800

    a reverseint solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let numStr = n.toString().split('').reverse().join('');
+
+  return numStr * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+
+function reverseInt(n) {
+  let numStr = n.toString();
+  
+  if (n >= 0) {
+    if (numStr.length === 1) {
+      return n;
+    } else {
+      return Number(numStr.split('').reverse().join(''));
+    }
+  } else {
+    let numCopy = numStr.slice(1);
+    let res = '-' + numCopy.split('').reverse().join('');
+    return Number(res)
+  }
+}
