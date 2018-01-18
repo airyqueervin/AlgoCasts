a605fa902e702aeb0e6d8ba6567272fc1880f79c
Hack Reactor Student <student@hackreactor.com>
2018-01-18 14:27:52 -0800

    a resverse string solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+reverse('hello')
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   let res = '';
+//   let strLen = str.length-1;
+//   for (let i = strLen; i >= 0; i--) {
+//     res+=str[i]
+//   }
+//   return res; 
+// }
+
+// function reverse(str) {
+//   let res = '';
+
+//   for (let char of str) {
+//     console.log(char)
+//     res = char + res;
+//   } 
+//   return res;
+// }
