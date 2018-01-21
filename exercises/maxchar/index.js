2b05a2e5099aa1fe963a5d21470b9653c0a3dccb
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-01-20 21:49:13 -0800

    a solution for maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let chars = {};
+  let count = 0;
+  let res;
+  for (char of str) {
+    chars[char] = chars[char]++ || 1;
+  }
+  for (char in chars) {
+    if (chars[char] > count) {
+      count = chars[char];
+      res = char;
+    }
+  }
+  return res;
+}
 
 module.exports = maxChar;
