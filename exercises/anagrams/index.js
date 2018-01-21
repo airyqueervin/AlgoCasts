ec9f6c8ed8c08b311c33d09aedd9cba52d91a725
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-01-20 23:15:44 -0800

    a working solution for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,23 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let strAMap = {};
+  let strBMap = {};
+  let strA = stringA.toLowerCase();
+  let strB = stringB.toLowerCase();
+  for (let charA of strA) {
+    strAMap[charA] = strAMap[charA]++ || 1;
+  }
+  for (let charB of strB) {
+    strBMap[charB] = strBMap[charB]++ || 1;
+  }
+  for (let char in strBMap) {
+    if (strAMap[char] !== strBMap[char]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = anagrams;
