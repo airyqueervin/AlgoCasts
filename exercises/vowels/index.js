82bd659debc7a5d8fdb3120ca19b3dd5628b46cb
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-01-30 16:17:18 -0800

    iterative solution for vowels.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // create a vowel hash
+  const vowelHash = {
+    a: 'a',
+    e: 'e',
+    i: 'i',
+    o: 'o',
+    u: 'u'
+  };
+  // create a counter
+  let vowelCount = 0;
+  // iterate over the string
+  for (char of str) {
+    // check if char is in the hash
+    if (vowelHash[char.toLowerCase()]) {
+      // icrement the counter
+      vowelCount++;
+    }
+  }
+  // return count 
+  return vowelCount;
+}
 
 module.exports = vowels;
