e03b48a0fb762161fe1157afd0de1c35d4b90c57
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-02-13 15:58:55 -0800

    tree node, df, and bf code working

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,64 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    // needs to add a data
+    this.children.push(new Node(data));
+  }
+  
+  remove(data) {
+    // iterate over the children
+      // filter out if they match the data then remove it
+    this.children = this.children.filter(node => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(cb) {
+    // start at root node
+    let queue = [this.root]
+    let node;
+
+    while (node = queue.shift()) {
+      if (node.children) {
+        queue.push(...node.children);
+        // for (let child of node.children) {
+        //   // console.log('child', child);
+        //   queue.push(child)
+        // }
+      }
+      cb(node)
+    }
+  }
+  traverseDF(cb, node = this.root) {
+    // start at the root
+    // use recursion
+      // if there are no children return
+        // cb(node)
+    // else recurse
+
+    cb(node)
+    if (!node.children) {
+      return
+    } else {
+      for (let child of node.children) {
+        this.traverseDF(cb, child)
+      }
+    }
+  }
+
+}
 
 module.exports = { Tree, Node };
